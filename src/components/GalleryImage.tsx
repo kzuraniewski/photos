8bdfb1fb0239ea399daa90a18977b9e97cde58b6
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-01-14 04:34:59 +0100

    image loading state

--- src/components/GalleryImage.tsx
+++ src/components/GalleryImage.tsx
@@ -1,18 +1,34 @@
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import Image from 'next/image';
+import { useState } from 'react';
+
+const IMAGE_SIZE = 250;
 
 export type GalleryImageProps = {
 	src: string;
 };
 
 const GalleryImage = ({ src }: GalleryImageProps) => {
+	const [isLoading, setIsLoading] = useState(true);
+
 	return (
-		<Box sx={{ position: 'relative', width: 250, height: 250 }}>
+		<Box
+			sx={{ position: 'relative', width: IMAGE_SIZE, height: IMAGE_SIZE }}
+		>
+			{isLoading && (
+				<Skeleton
+					variant="rectangular"
+					width={IMAGE_SIZE}
+					height={IMAGE_SIZE}
+				/>
+			)}
+
 			<Image
 				src={src}
 				alt="Gallery image"
 				fill
 				style={{ objectFit: 'cover' }}
+				onLoad={() => setIsLoading(false)}
 			/>
 		</Box>
 	);
